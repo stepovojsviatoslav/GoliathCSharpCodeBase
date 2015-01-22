83fac3cba8297bd1c2880f5fecfbd68b90d0428d
PirateTigo <piratetigo@gmail.com>
2015-01-22 17:31:17 +0600

    intermediate commit

--- CSharpCodeBase/entities/player/player.cs
+++ CSharpCodeBase/entities/player/player.cs
@@ -1,38 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using MainGame.core;
+//using UnityEngine.EventSystems;
  
- using System.Collections.Generic;
- 
- using System.Linq;
- 
- using System.Text;
- 
- using UnityEngine;
- 
- using System.Collections;
- 
- using MainGame.core;
- 
- using UnityEngine.EventSystems;
- 
- namespace MainGame{
- public class player :unityexistsentity{
- public void init (self, gameObject);
-       UnityExistsEntity.init(self, gameObject);
-       self:AddComponent(PlayerController);
-       self:AddComponent(GuiControl);
-       this.luaMapper:SetAlwaysVisible(true);
-       this.static = false;
- }})
- public void Update(){
-   UnityExistsEntity.Update(self);
- }
- public void FixedUpdate(){
-   UnityExistsEntity.FixedUpdate(self);
- }
- public void LateUpdate(){
-   UnityExistsEntity.LateUpdate(self)  ;
- }
- public void StoreHomePosition(position){
-   this.homePosition = position;
- }
- }}
+namespace MainGame{
+    public class Player : UnityEntity
+    {
+        Player(GameObject gameObject)
+        {
+            base(gameObject);
+            bool static = false;
+        }
+        //public void init (self, gameObject);
+        //      UnityExistsEntity.init(self, gameObject);
+        //      self:AddComponent(PlayerController);
+        //      self:AddComponent(GuiControl);                  
+        //}})
+        public void StoreHomePosition(Vector3 position)
+        {
+            homePosition = position;
+        }         
+    }
+}
